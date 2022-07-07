cdbbd214bd7a8bf71aa1b4406a43a4a882a8f7ec
Jacob Sumner <sumnerjacob23@gmail.com>
2022-07-06 21:13:43 -0600

    Revised

--- my_prework.py
+++ my_prework.py
@@ -1,14 +1,12 @@
 # question 1
 #Write a function to print"hello_USERNAME!"
 
-from sys import get_coroutine_origin_tracking_depth
-
 
 def hello_name(user_name):
     print("hello, " + user_name.upper() + "!" )
 
 hello_name("gnarf")    
-#not actually sure of how to put the function answer in " "
+
 #question 2
 #Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
 def first_odds():
@@ -23,7 +21,6 @@ first_odds()
 
 #question 3
 #Please write a Python function, max_num_in_list to return the maxnumber of a given list
-
 def max_num_in_list(a_list):
     print(max(a_list))
 a_list = [13, 97, 23, 65, 401, 3, 299]
@@ -43,6 +40,7 @@ def is_leap_year(a_year):
 print(is_leap_year(2012))            
 
 #question5
+#Write a function to check to see if all numbers in list are consecutive numbers
 def is_consecutive(a_list):
     return sorted(a_list) == list(range(min(a_list), max(a_list)+1))
 num = [9,7,3,4,6,5,]
